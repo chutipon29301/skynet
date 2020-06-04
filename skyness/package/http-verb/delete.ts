2481f0583a744029ef23a6693736105b9b4ce401
YanisaS <yanisas@pttep.com>
2020-06-04 22:46:15 +0700

    ✨ edit method decorator

--- skyness/package/http-verb/delete.ts
+++ skyness/package/http-verb/delete.ts
@@ -5,12 +5,11 @@ export interface Delete {
   export interface DeleteConfig {}
   
   export function Delete(path: string, config?: DeleteConfig) {
-    return function classDecorator<T extends { new (...args: any[]): {} }>(
-      constructor: T
-    ): { new (...args: any[]): Delete } {
-      return class extends constructor implements Delete {
-        path = path;
-      };
+    return async function (target: any, propertyKey: string, descriptor: PropertyDescriptor) {
+        console.log("Path:",path)
+        console.log("Target:",target)
+        console.log("Proprety Key:",propertyKey)
+        console.log("Descriptor:", await descriptor.value());
     };
   }
   
--- skyness/package/http-verb/post.ts
+++ skyness/package/http-verb/post.ts
@@ -5,12 +5,11 @@ export interface Post {
   export interface PostConfig {}
   
   export function Post(path: string, config?: PostConfig) {
-    return function classDecorator<T extends { new (...args: any[]): {} }>(
-      constructor: T
-    ): { new (...args: any[]): Post } {
-      return class extends constructor implements Post {
-        path = path;
-      };
+    return async function (target: any, propertyKey: string, descriptor: PropertyDescriptor) {
+        console.log("Path:",path)
+        console.log("Target:",target)
+        console.log("Proprety Key:",propertyKey)
+        console.log("Descriptor:", await descriptor.value());
     };
   }
   
