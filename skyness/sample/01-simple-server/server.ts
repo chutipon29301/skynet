e3ca53d12e6a0441d162e986b797c57b834633b9
YanisaS <yanisas@pttep.com>
2020-06-04 22:46:39 +0700

    ✨ add example in method decorator

--- skyness/sample/01-simple-server/server.ts
+++ skyness/sample/01-simple-server/server.ts
@@ -2,6 +2,7 @@ import { Module } from "../../package/core/module.ts";
 import { Service } from "../../package/core/service.ts";
 import { Router } from "../../package/core/router.ts";
 import { Server } from "../../package/core/runner.ts";
+import { Get } from "../../package/http-verb/get.ts";
 
 @Service({})
 export class ApplicationService {}
@@ -17,3 +18,10 @@ export class ApplicationModule {}
 
 const application = new Server(ApplicationModule);
 await application.listen();
+
+class C {
+  @Get("ming")
+  public async test(param: string) {
+    return "hello ming"
+  }
+}
