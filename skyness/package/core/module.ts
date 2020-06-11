83b2864b4ac1773ceccea76cf97e08cb35426fdb
Chutipon Hirankanokkul <chutiponh@hotmail.com>
2020-06-11 20:49:58 +0700

    ✨ Add service options for module

--- skyness/package/core/module.ts
+++ skyness/package/core/module.ts
@@ -1,7 +1,9 @@
 import { Router } from "./router.ts";
+import { Service } from "./service.ts";
 
 export interface Application {
   routers: Router[];
+  services: Service[];
 }
 
 export interface Module {}
@@ -9,20 +11,17 @@ export interface Module {}
 export interface ModulesOptions {
   services?: any[];
   routers?: any[];
-  imports?: any[];
-  exports?: any[];
 }
 
 export function Module(option: ModulesOptions) {
-  let routers: Router[] = [];
-  if (option.routers) {
-    routers = option.routers.map((o) => new o());
-  }
+  let routers = (option.routers ?? []).map((o) => new o());
+  let services = (option.services ?? []).map((o) => new o());
   return function classDecorator<T extends { new (...args: any[]): {} }>(
     constructor: T
   ): { new (...args: any[]): Application } {
     return class extends constructor implements Application {
       routers = routers;
+      services = services;
     };
   };
 }
--- skyness/package/core/service.ts
+++ skyness/package/core/service.ts
@@ -1,3 +1,7 @@
+export interface Service {
+  
+}
+
 export function Service(option: any) {
   return function classDecorator<T extends { new (...args: any[]): {} }>(
     constructor: T
